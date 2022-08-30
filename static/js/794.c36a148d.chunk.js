33627c1a54cae54987cd86c633ce9bdf2f9e0c6c
serj-nobody <81528207+serj-nobody@users.noreply.github.com>
2022-08-30 12:16:11 +0000

    Deploying to gh-pages from @ serj-nobody/goit-react-hw-05-movies@0ef7f95d56e7d8190469143dd4db74dbca17af95 🚀

---
+++ static/js/794.c36a148d.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[794],{1794:function(e,r,t){t.r(r);var n=t(501),o=t(184);r.default=function(){return(0,o.jsxs)("div",{children:[(0,o.jsx)("h2",{children:"Oops, page not found!"}),(0,o.jsxs)("p",{children:["proceed to ",(0,o.jsx)(n.rU,{to:"/",children:"Home Page"})]})]})}}}]);
+//# sourceMappingURL=794.c36a148d.chunk.js.map
